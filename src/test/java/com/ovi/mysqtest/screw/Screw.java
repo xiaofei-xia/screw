ea263d6b41f4e2f053d4951bc05838230e46c446
yangxiaofei <729994995@qq.com>
2021-04-30 14:40:46 +0800

    first

---
+++ src/test/java/com/ovi/mysqtest/screw/Screw.java
@@ -0,0 +1,83 @@
+package com.ovi.mysqtest.screw;
+
+import cn.smallbun.screw.core.Configuration;
+import cn.smallbun.screw.core.engine.EngineConfig;
+import cn.smallbun.screw.core.engine.EngineFileType;
+import cn.smallbun.screw.core.engine.EngineTemplateType;
+import cn.smallbun.screw.core.execute.DocumentationExecute;
+import cn.smallbun.screw.core.process.ProcessConfig;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.junit.jupiter.api.Test;
+
+
+import javax.sql.DataSource;
+import java.util.ArrayList;
+
+/**
+ * @author yxfstart
+ * @create 2020-07-06 13:11
+ */
+public class Screw {
+
+    /**
+     * 文档生成
+     */
+    @Test
+    public void documentGeneration() {
+        //数据源
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        hikariConfig.setJdbcUrl("jdbc:mysql://rm-uf6m82ql0d47777794o.mysql.rds.aliyuncs.com:3306/sport_common");
+        hikariConfig.setUsername("orbbec");
+        hikariConfig.setPassword("Ovi@2020");
+        //设置可以获取tables remarks信息
+        hikariConfig.addDataSourceProperty("useInformationSchema", "true");
+        hikariConfig.setMinimumIdle(2);
+        hikariConfig.setMaximumPoolSize(5);
+        DataSource dataSource = new HikariDataSource(hikariConfig);
+        //生成配置
+        EngineConfig engineConfig = EngineConfig.builder()
+                //生成文件路径
+                .fileOutputDir("D:\\files\\ovi")
+                //打开目录
+                .openOutputDir(true)
+                //文件类型
+                //.fileType(EngineFileType.HTML)
+                .fileType(EngineFileType.WORD)
+                //生成模板实现
+                .produceType(EngineTemplateType.freemarker).build();
+
+        //忽略表
+        ArrayList<String> ignoreTableName = new ArrayList<>();
+        ignoreTableName.add("test_user");
+        ignoreTableName.add("test_group");
+        //忽略表前缀
+        ArrayList<String> ignorePrefix = new ArrayList<>();
+        ignorePrefix.add("test_");
+        //忽略表后缀
+        ArrayList<String> ignoreSuffix = new ArrayList<>();
+        ignoreSuffix.add("_test");
+        ProcessConfig processConfig = ProcessConfig.builder()
+                //忽略表名
+                .ignoreTableName(ignoreTableName)
+                //忽略表前缀
+                .ignoreTablePrefix(ignorePrefix)
+                //忽略表后缀
+                .ignoreTableSuffix(ignoreSuffix).build();
+        //配置
+        Configuration config = Configuration.builder()
+                //版本
+                .version("1.0.0")
+                //描述
+                .description("数据库设计文档生成")
+                //数据源
+                .dataSource(dataSource)
+                //生成配置
+                .engineConfig(engineConfig)
+                //生成配置
+                .produceConfig(processConfig).build();
+        //执行生成
+        new DocumentationExecute(config).execute();
+    }
+}
